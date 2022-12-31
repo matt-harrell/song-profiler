9acfe0dd90783579c576876f4db61827027e34ad
Matt Harrell <matthew.629@hotmail.com>
2022-12-31 16:14:17 -0500

    remove reference to open state

--- src/components/MenuBar/MenuBar.tsx
+++ src/components/MenuBar/MenuBar.tsx
@@ -1,10 +1,9 @@
-import { useDispatch, useSelector } from "react-redux";
-import { selectMenuOpen, toggleMenu } from "../../slices/menuSlice";
+import { useDispatch } from "react-redux";
+import { toggleMenu } from "../../slices/menuSlice";
 import MenuBarComp from "./MenuBarComp";
 
 const MenuBar = () => {
     const dispatch = useDispatch();
-    const open = useSelector(selectMenuOpen);
     const handleClick = () => {
         dispatch(toggleMenu(true));
     }
