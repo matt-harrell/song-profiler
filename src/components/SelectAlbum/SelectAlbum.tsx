6bf57a7ffa2ac1fb992143c27cd3a958f6bb08c1
Matt Harrell <matthew.629@hotmail.com>
2023-01-04 00:24:22 -0500

    add click handlers to change currentAlbum

--- src/components/SelectAlbum/SelectAlbum.tsx
+++ src/components/SelectAlbum/SelectAlbum.tsx
@@ -1,8 +1,24 @@
+import { useDispatch } from "react-redux";
+import { nextAlbum, prevAlbum } from "../../slices/spotifySlice";
 import SelectAlbumComp from "./SelectAlbumComp";
 
 const SelectAlbum = () => {
+
+    const dispatch = useDispatch();
+
+    const handleNextClick = () => {
+        dispatch(nextAlbum());
+    }
+
+    const handlePrevClick = () => {
+        dispatch(prevAlbum());
+    }
+
     return(
-        <SelectAlbumComp/>
+        <SelectAlbumComp
+            handleNextClick={handleNextClick}
+            handlePrevClick={handlePrevClick}
+        />
     );
 }
 
--- src/components/SelectAlbum/SelectAlbumComp.tsx
+++ src/components/SelectAlbum/SelectAlbumComp.tsx
@@ -4,7 +4,12 @@ import ArrowRightIcon from '@mui/icons-material/ArrowRight';
 import { useSelector } from "react-redux";
 import { selectCurrentAlbum, selectTracks } from "../../slices/spotifySlice";
 
-const SelectAlbumComp = () => {
+interface props {
+    handleNextClick: () => void
+    handlePrevClick: () => void
+}
+
+const SelectAlbumComp = ({handleNextClick,handlePrevClick}:props) => {
 
    const tracks = useSelector(selectTracks);
    const curretAlbum = useSelector(selectCurrentAlbum);
@@ -12,7 +17,12 @@ const SelectAlbumComp = () => {
     return(
         <Grid container spacing={2}>
             <Grid item xs={2} sx={{display:'flex',flexDirection:'column',justifyContent:'center'}}>
-                <IconButton color="primary" aria-label="previos">
+                <IconButton 
+                    color="primary" 
+                    aria-label="previos"
+                    onClick={handlePrevClick}
+                    disabled={curretAlbum === 0 ? true : false}
+                >
                     <ArrowLeftIcon fontSize="large"/>
                 </IconButton>
             </Grid>
@@ -26,7 +36,9 @@ const SelectAlbumComp = () => {
             <Grid item xs={2} sx={{display:'flex',flexDirection:'column',justifyContent:'center'}}>
                 <IconButton 
                     color="primary" 
-                    aria-label="next" 
+                    aria-label="next"
+                    onClick={handleNextClick}
+                    disabled={curretAlbum === tracks.length-1 ? true : false} 
                 >
                     <ArrowRightIcon fontSize="large"/>
                 </IconButton>
