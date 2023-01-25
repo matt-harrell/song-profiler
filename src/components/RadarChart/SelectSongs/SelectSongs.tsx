dc59f544c856c5dbf4f1ff8f6256095c4b844eb1
Matt Harrell <matthew.629@hotmail.com>
2023-01-24 21:51:24 -0500

    set muliple select auto complete

--- src/components/RadarChart/SelectSongs/SelectSongs.tsx
+++ src/components/RadarChart/SelectSongs/SelectSongs.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { SyntheticEvent, useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { selectTracks } from "../../../slices/spotifySlice";
 import SelectSongsComp from "./SelectSongsComp";
@@ -7,18 +7,26 @@ const SelectSongs = () => {
     
     const tracks = useSelector(selectTracks)
     const [songTitles, setSongTitles] = useState<string[]>([])
+    const [value, setValue] = useState<string[]>([]);
 
     useEffect(() => {
         const trackTitles = tracks.map(track => track.name)
-
-        setSongTitles([...trackTitles])
         trackTitles.unshift('All Songs')
 
+        setSongTitles([...trackTitles])
+        setValue([trackTitles[0]])
+
     },[tracks])
+
+    const handleChange = (e:SyntheticEvent<Element, Event>,songs:string[]) => {
+        setValue([...songs]);
+    }
 
     return(
         <SelectSongsComp
             songTitles={songTitles}
+            value={value}
+            handleChange={handleChange}
         />
     );
 }
--- src/components/RadarChart/SelectSongs/SelectSongsComp.tsx
+++ src/components/RadarChart/SelectSongs/SelectSongsComp.tsx
@@ -1,17 +1,13 @@
 import { Autocomplete, TextField } from "@mui/material";
-import { useSelector } from "react-redux";
-import { selectTracks } from "../../../slices/spotifySlice";
+import { SyntheticEvent } from "react";
 
 interface props{
-    songTitles:string[];
+    songTitles:string[],
+    value:string[],
+    handleChange: (e: SyntheticEvent<Element, Event>, songs: string[]) => void,
 }
 
-const SelectSongsComp = ({songTitles}:props) => {
-
-    const tracks = useSelector(selectTracks);
-
-    
-
+const SelectSongsComp = ({songTitles,value,handleChange}:props) => {
 
     return(
         <Autocomplete
@@ -20,12 +16,15 @@ const SelectSongsComp = ({songTitles}:props) => {
             options={songTitles}
             getOptionLabel={(option) => option}
             defaultValue={[songTitles[0]]}
+            value={value}
+            sx={{maxWidth:'80%'}}
+            onChange={handleChange}
             renderInput={(params) => (
                 <TextField
                     {...params}
                     variant="standard"
                     label="Multiple values"
-                    placeholder="Favorites"
+                    placeholder="Songs"
                 />
             )}
         />
