9159650e84ce5b4674a0e855e0cdbe7b144a1acd
Matt Harrell <matthew.629@hotmail.com>
2023-01-25 21:04:17 -0500

    add logic for add songs to radar data

--- src/components/RadarChart/SelectSongs/SelectSongs.tsx
+++ src/components/RadarChart/SelectSongs/SelectSongs.tsx
@@ -1,10 +1,12 @@
 import { SyntheticEvent, useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { setRadarData } from "../../../slices/radarChartSlice";
 import { selectTracks } from "../../../slices/spotifySlice";
 import SelectSongsComp from "./SelectSongsComp";
 
 const SelectSongs = () => {
-    
+
+    const dispatch = useDispatch();
     const tracks = useSelector(selectTracks)
     const [songTitles, setSongTitles] = useState<string[]>([])
     const [value, setValue] = useState<string[]>([]);
@@ -19,7 +21,45 @@ const SelectSongs = () => {
     },[tracks])
 
     const handleChange = (e:SyntheticEvent<Element, Event>,songs:string[]) => {
+        let songsToPass = [];
         setValue([...songs]);
+
+        if(songs.includes('All Songs')){
+
+            const findAverage = (array:number[]) => Math.round(array.reduce((accumulator:number,currentValue:number) => {
+                const add = accumulator + currentValue
+                return add;
+            }) / array.length)
+
+            const acousticnessValues = tracks.map(track => track.acousticness);
+            const danceabilityValues = tracks.map(track => track.danceability);
+            const energyValues = tracks.map(track => track.energy);
+            const loudnessValues = tracks.map(track => track.loudness);
+            const valenceValues = tracks.map(track => track.valence);
+            songsToPass.push({
+                acousticness: findAverage(acousticnessValues),
+                danceability: findAverage(danceabilityValues),
+                energy:findAverage(energyValues),  
+                loudness:findAverage(loudnessValues),  
+                valence:findAverage(valenceValues),
+            })
+        }
+
+        songs.forEach((song) => {
+            if(song !== "All Songs"){
+                const songToAdd = tracks.find(track => track.name === song);
+                songsToPass.push({
+                    acousticness: songToAdd?.acousticness,
+                    danceability: songToAdd?.danceability,
+                    energy:songToAdd?.energy,  
+                    loudness:songToAdd?.loudness,  
+                    valence:songToAdd?.valence,
+                })
+            }
+        })
+
+        dispatch(setRadarData(songsToPass))
+        
     }
 
     return(
