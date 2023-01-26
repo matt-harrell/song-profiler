11ef95f142d1480fc9665887c46093e94e18e68b
Matt Harrell <matthew.629@hotmail.com>
2023-01-25 21:11:39 -0500

    remove uneed data state

--- src/components/RadarChart/RadarChart.tsx
+++ src/components/RadarChart/RadarChart.tsx
@@ -2,7 +2,7 @@ import { NumberValue, scaleLinear } from "d3";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setNumOfTracks } from "../../slices/filterButtonsSlice";
-import { selectCurrentTrack, selectHeight, selectWidth, setRadarData, selectData, setshowRadarChart } from "../../slices/radarChartSlice";
+import { selectCurrentTrack, selectHeight, selectWidth, setRadarData, setshowRadarChart } from "../../slices/radarChartSlice";
 import { selectTracks, setCurrentAlbum } from "../../slices/spotifySlice";
 import Axes from "./Axes";
 import DataPoints from "./DataPoints";
@@ -14,7 +14,6 @@ const RadarChartComp = () => {
     const width = useSelector(selectWidth);
     const height = useSelector(selectHeight);
     const features = ["acousticness","danceability","energy","loudness", "valence"];
-    const data = useSelector(selectData);
 
     useEffect(() => {
         dispatch(setshowRadarChart(true))
--- src/slices/radarChartSlice.ts
+++ src/slices/radarChartSlice.ts
@@ -1,14 +1,3 @@
-/* need slice to following state
-- width
-- height
-- handle which data is displaying
-- handle how to change which data is displaying
-    - find the average off all songs if all songs is showing
-    - if it is a single song, then use those values
-- handle resizing the width and height when the screen size changes
-- handle changing the data
-*/
-
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 interface FeatureData {
@@ -18,19 +7,11 @@ interface FeatureData {
     loudness:number,  
     valence:number,
 }
-
-interface RadarChartData {
-    nameOfTrack:string,
-    featureData:FeatureData,
-}
-
 interface RadarChartState {
     showRadarChart:boolean,
     width:number,
     height:number,
     currentTrack:string,
-    // if current track is === to "All Songs" then that is when we need to find average
-    data:RadarChartData[],
     radarData:FeatureData[];
 }
 
@@ -53,55 +34,6 @@ const radarChartSlice = createSlice({
         setCurrentTrack(state,action:PayloadAction<string>){
             state.currentTrack = action.payload;
         },
-        addData(state,action){
-            if (state.currentTrack === 'All Songs') {
-
-                const findAverage = (array:number[]) => Math.round(array.reduce((accumulator:number,currentValue:number) => {
-                    const add = accumulator + currentValue
-                    return add;
-                }) / array.length)
-
-
-                state.data.push({
-                    nameOfTrack:'All Songs',
-                    featureData:{
-                        acousticness: findAverage(action.payload.acousticness),
-                        danceability: findAverage(action.payload.danceability),
-                        energy:findAverage(action.payload.energy),  
-                        loudness:findAverage(action.payload.loudness),  
-                        valence:findAverage(action.payload.valence),
-                    }
-                })
-
-                state.radarData.push({
-                        acousticness: findAverage(action.payload.acousticness),
-                        danceability: findAverage(action.payload.danceability),
-                        energy:findAverage(action.payload.energy),  
-                        loudness:findAverage(action.payload.loudness),  
-                        valence:findAverage(action.payload.valence),
-                })
-
-            } else {
-                state.data.push({
-                    nameOfTrack:action.payload.nameOfTrack,
-                    featureData:{
-                        acousticness: action.payload.acousticness,
-                        danceability: action.payload.danceability,
-                        energy:action.payload.energy,  
-                        loudness:action.payload.loudness,  
-                        valence:action.payload.valence,
-                    }
-                })
-
-                state.radarData.push({
-                        acousticness: action.payload.acousticness,
-                        danceability: action.payload.danceability,
-                        energy:action.payload.energy,  
-                        loudness:action.payload.loudness,  
-                        valence:action.payload.valence,
-                })
-            }
-        },
         setRadarData(state,action){
             state.radarData = action.payload;
         }
@@ -115,7 +47,6 @@ export const selectShowRadarChart = (state: { RadarChart: { showRadarChart: bool
 export const selectWidth = (state: { RadarChart: { width: number; }; }) => state.RadarChart.width;
 export const selectHeight = (state: { RadarChart: { height: number; }; }) => state.RadarChart.height;
 export const selectCurrentTrack = (state: { RadarChart: { currentTrack: string; }; }) => state.RadarChart.currentTrack;
-export const selectData = (state: { RadarChart: { data: RadarChartData[]; }; }) => state.RadarChart.data;
 export const selectRadarData = (state: { RadarChart: { radarData: FeatureData[]; }; }) => state.RadarChart.radarData;
 
 export default radarChartSlice.reducer;
