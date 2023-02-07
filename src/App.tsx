e4dd2841b5b8cf0a83603facaf0e2b8fb1c1df08
Matt Harrell <matthew.629@hotmail.com>
2023-02-06 22:04:16 -0500

    add merged drawer to app

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,7 @@ import LoginButton from './components/LoginButton/LoginButton';
 import MenuBar from './components/MenuBar/MenuBar';
 import { fetchTopTracks, selectIsLoggedIn, setIsLoggedIn,selectLoading } from './slices/spotifySlice';
 import { selectThemeColors } from './slices/ThemeSlice';
-import BarChartMenuDrawer from './components/BarChartMenuDrawer/BarChartMenuDrawer';
-import { selectShowRadarChart } from './slices/radarChartSlice';
-import RadarMenuDrawer from './components/RadarMenuDrawer/RadarMenuDrawer';
+import Drawer from './components/Drawer/Drawer';
 
 function App() {
 
@@ -18,7 +16,6 @@ function App() {
   const isLoggedIn = useSelector(selectIsLoggedIn);
   const themeColors = useSelector(selectThemeColors);
   const isLoading = useSelector(selectLoading);
-  const showRadarChart = useSelector(selectShowRadarChart);
 
   useEffect(() => {
     const accessToken = window.location.href.match(/access_token=([^&]*)/); 
@@ -33,7 +30,7 @@ function App() {
     <ThemeComp>
       <div className="App" style={{ backgroundColor: `${isLoading ? 'white' : themeColors.backgroundColor.color}` }}>
         <MenuBar/>
-        {showRadarChart ? <RadarMenuDrawer/> : <BarChartMenuDrawer/>}
+        <Drawer/>
         {isLoggedIn ? <LoggedInScreen /> : <LoginButton />}
       </div>
     </ThemeComp>
