849de5da64e6d215ca6a10b60b816cc5d2aed752
Matt Harrell <matthew.629@hotmail.com>
2023-02-20 23:19:17 -0500

    modify functions to just get varible

--- netlify/functions/accessAPIKey.ts
+++ netlify/functions/accessAPIKey.ts
@@ -1,16 +1,18 @@
 import { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
 
 const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
+  
   try {
-    const response = await fetch(`${process.env.API_KEY}`);
     return{
         statusCode:200,
-        body:JSON.stringify(response)
+        body:JSON.stringify(process.env.API_KEY)
     }
   } catch (error) {
+    console.log(error);
+    
     return {
         statusCode: 404,
-        body: error.toString(),
+        body: error.toString()
       };
   }
 };
